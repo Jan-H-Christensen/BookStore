7d38cb5222002ec33de41b4cf1a0941efac4987f
Jan_H_Christensen <jan.christensen1981@gmail.com>
2024-12-04 13:42:25 +0100

    ✨ added try catch to sql service

--- BookStoreConApp/DbServices/SqlService.cs
+++ BookStoreConApp/DbServices/SqlService.cs
@@ -18,51 +18,109 @@ namespace BookStoreConApp.DbServices
 
         public Costumers Login(string name, string email)
         {
-            _sqlContext.Database.EnsureCreated();
-            return _sqlContext.Costumers.Where(c => c.name == name && c.email == email).FirstOrDefault();
+            try
+            {
+                _sqlContext.Database.EnsureCreated();
+                return _sqlContext.Costumers.Where(c => c.name == name && c.email == email).FirstOrDefault();
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("costumer not found");
+                throw;
+            }
         }
 
 
         public List<Books> GetBooks()
         {
-            _sqlContext.Database.EnsureCreated();
-            return _sqlContext.Books.ToList();
+            try
+            {
+                _sqlContext.Database.EnsureCreated();
+                return _sqlContext.Books.ToList();
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("books not found");
+                throw;
+            }
         }
 
         public void CreateOrder(Orders order)
         {
-            _sqlContext.Database.EnsureCreated();
-            if (order != null)
+            try
             {
-                _sqlContext.Orders!.Add(order);
-                UpdateInventoryByPurchaseOrder(order.order_id, order.total_amount);
-                UpdateBookStockLevel(order.order_id, order.total_amount);
-                _sqlContext.SaveChanges();
+                _sqlContext.Database.EnsureCreated();
+                if (order != null)
+                {
+                    _sqlContext.Orders!.Add(order);
+                    UpdateInventoryByPurchaseOrder(order.order_id, order.total_amount);
+                    UpdateBookStockLevel(order.order_id, order.total_amount);
+                    _sqlContext.SaveChanges();
+                }
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("order not created");
+                throw;
             }
         }
 
         public void CreateOrderDetail(OrderDetails orderDetail)
         {
+            try
+            {
 
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("order detail not created");
+                throw;
+            }
         }
 
         public void UpdateInventory(Inventory inventory)
         {
-
+            try
+            {
+                _sqlContext.Database.EnsureCreated();
+                _sqlContext.Inventory!.Add(inventory);
+                _sqlContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("inventory not updated");
+                throw;
+            }
         }
 
         private void UpdateInventoryByPurchaseOrder(int bookId, int quantity)
         {
-            _sqlContext.Database.EnsureCreated();
-            _sqlContext.Inventory.Where(i => i.book_id == bookId).FirstOrDefault().stock_level -= quantity;
-            _sqlContext.SaveChanges();
+            try
+            {
+                _sqlContext.Database.EnsureCreated();
+                _sqlContext.Inventory.Where(i => i.book_id == bookId).FirstOrDefault().stock_level -= quantity;
+                _sqlContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("failed to update inventory by purchase order");
+                throw;
+            }
         }
 
         private void UpdateBookStockLevel(int bookId, int quantity)
         {
-            _sqlContext.Database.EnsureCreated();
-            _sqlContext.Books.Where(i => i.book_id == bookId).FirstOrDefault().stock_level -= quantity;
-            _sqlContext.SaveChanges();
+            try
+            {
+                _sqlContext.Database.EnsureCreated();
+                _sqlContext.Books.Where(b => b.book_id == bookId).FirstOrDefault().stock_level -= quantity;
+                _sqlContext.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("failed to update book stock level by purchase order");
+                throw;
+            }
         }
     }
 }
